b192fe1e1cd6c5f8b4ac4eb0334950ce3910a656
sadiqulfakir <sadikul0140@gmail.com>
2022-03-25 18:05:15 +0600

    all-add

---
+++ script.js
@@ -0,0 +1,4 @@
+function sidebarHandler(e) {
+	const sideBar = document.getElementById('sidebar').classList.toggle('hidden');
+	// const sideBar = document.getElementById('sidebar').style.display = "block";
+}
